42d18bc8d55f8bb4a64ae090a3e918574003ece4
Seb P <me@sebpiller.ch>
2021-02-25 23:41:15 +0100

    .

--- custom_components/lukeroberts_lampf/light.py
+++ custom_components/lukeroberts_lampf/light.py
@@ -2,14 +2,13 @@
 
 import logging
 
-import voluptuous as vol
-
 import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 # Import the device class from the component that you want to support
 from homeassistant.components.light import (
-    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, LightEntity)
+    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, LightEntity, SUPPORT_BRIGHTNESS, SUPPORT_COLOR_TEMP, SUPPORT_COLOR,
+    SUPPORT_EFFECT, SUPPORT_FLASH, SUPPORT_TRANSITION)
 from homeassistant.const import CONF_HOST
-import awesomelights
 
 from .lampf_bt import LampFBle
 
@@ -24,6 +23,9 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up Tuya light platform."""
+    if discovery_info is None:
+        return
     """Set up the Awesome Light platform."""
     # Assign configuration variables.
     # The configuration check takes care they are present.
@@ -40,16 +42,16 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     #     return
 
     # Add devices
-    add_entities(LampFLight(light) for light in hub.lights())
+    add_entities(LampFLight(LampFBle()))
 
 
 class LampFLight(LightEntity):
     """Representation of an Awesome Li§ght."""
 
-    def __init__(self):
+    def __init__(self, lampf: LampFBle):
         """Initialize an AwesomeLight."""
-        self._light = LampFBle()
-        self._name = "dfgsdg"
+        self._light = lampf
+        self._name = "LampF"
         self._state = None
         self._brightness = None
 
@@ -58,14 +60,29 @@ class LampFLight(LightEntity):
         """Return the display name of this light."""
         return self._name
 
+    # brightness	int	None	Return the brightness of this light between 0..255
+    # color_temp	int	None	Return the CT color value in mireds.
+    # effect	String	None	Return the current effect.
+    # effect_list	list	None	Return the list of supported effects.
+    # hs_color	list	None	Return the hue and saturation color value [float, float].
+    # is_on	bool	bool	Returns if the light entity is on or not.
+    # max_mireds	int	int	Return the warmest color_temp that this light supports.
+    # min_mireds	int	int	Return the coldest color_temp that this light supports.
+    # supported_features	int	int	Flag supported features.
+    # white_value	int	None	Return the white value of this light between 0..255.
+
     @property
     def brightness(self):
         """Return the brightness of the light.
         This method is optional. Removing it indicates to Home Assistant
         that brightness is not supported for this light.
         """
-        # return self._brightness
-        return self._light.bottom_brightness
+        return self._brightness
+
+    @brightness.setter
+    def brightness(self, b):
+        self._light.bottom_brightness = b
+        self._brightness = b
 
     @property
     def is_on(self):
@@ -78,7 +95,7 @@ class LampFLight(LightEntity):
         brightness control.
         """
         self._light.power = True
-        self._light.bottom_brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
+        self.brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
@@ -97,3 +114,14 @@ class LampFLight(LightEntity):
         self._light.update()
         self._state = self._light.power
         self._brightness = self._light.bottom_brightness
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        supports = SUPPORT_BRIGHTNESS
+        # supports |= SUPPORT_COLOR
+        # supports |= SUPPORT_COLOR_TEMP
+        # supports |= SUPPORT_EFFECT
+        # supports |= SUPPORT_FLASH
+        # supports |= SUPPORT_TRANSITION
+        return supports
